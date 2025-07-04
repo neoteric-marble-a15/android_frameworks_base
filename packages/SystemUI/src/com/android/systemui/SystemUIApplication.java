3956fe1e4926fc5059c41ab4044ffabc79184c32
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-05 06:43:05 +0800

    SystemUI: Add lockscreen media art
    Change-Id: I4db66c3e1e98c1ac04a315e8e76ec3799e8f4cbd
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/SystemUIApplication.java
+++ packages/SystemUI/src/com/android/systemui/SystemUIApplication.java
@@ -481,4 +481,15 @@ public class SystemUIApplication extends Application implements
 
         n.addExtras(extras);
     }
+
+    @NonNull
+    @Override
+    public WMComponent getWMComponent() {
+        return mInitializer.getWMComponent();
+    }
+
+    @NonNull
+    public SysUIComponent getSysUIComponent() {
+        return mSysUIComponent;
+    }
 }
