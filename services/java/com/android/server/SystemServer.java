035330582152f418f7c2b54ec1d976c18102fe91
ShadeDark6 <nizamfa6@gmail.com>
2025-10-26 17:41:26 +0000

    Revert "services: Add freeform system service"
    This reverts commit b0d534cda1ad4c0bf77fd69ccf9187c462f6f755.

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -162,7 +162,6 @@ import com.android.server.display.DcDimmingService;
 import com.android.server.devicestate.DeviceStateManagerService;
 import com.android.server.display.AutoAODService;
 import com.android.server.display.DisplayManagerService;
-import com.android.server.display.FreeformService;
 import com.android.server.display.color.ColorDisplayService;
 import com.android.server.dreams.DreamManagerService;
 import com.android.server.emergency.EmergencyAffordanceService;
@@ -2667,10 +2666,6 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("AppLockManagerService");
             mSystemServiceManager.startService(AppLockManagerService.Lifecycle.class);
-            t.traceEnd();
-
-            t.traceBegin("FreeformService");
-            mSystemServiceManager.startService(FreeformService.class);
             t.traceEnd();
 
             if (!isWatch) {
